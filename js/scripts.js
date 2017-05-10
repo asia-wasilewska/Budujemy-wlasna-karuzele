e5bdf96cf1c646db2a352171c9194b52d8c90a91
Asia Wasilewska <asia_wasilewska@tlen.pl>
2017-05-10 10:53:44 +0200

    First upload

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function(){ 
+
+  var carouselList = $("#carousel ul");
+  setInterval(changeSlide, 3000); 
+  
+  function moveFirstSlide() {
+    var firstItem = carouselList.find("li:first"); 
+    var lastItem = carouselList.find("li:last");
+    
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  }
+  
+  //change slide
+  
+  function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+  }
+
+  
+});
